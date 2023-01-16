2fd51f29763ed9bc7e77040b0ec0a6cbe0104714
Gary Shaw <garyshaw05@gmail.com>
2023-01-16 16:05:37 +0000

    react version

--- src/components/Screenshot.jsx
+++ src/components/Screenshot.jsx
@@ -1,17 +1,16 @@
 import React, { createRef } from "react";
 // import {disable-next-line} from "eslint";
 // import Text from "./Text";
-import { useScreenshot, createFileName } from "use-react-screenshot";
+// import { useScreenshot, createFileName } from "use-react-screenshot";
+import { createFileName } from "use-react-screenshot";
 
 const Screenshot = () => {
   const ref = createRef(null);
   // eslint-disable-next-line
-  const [image, takeScreenShot] = useScreenshot({
-    type: "image/jpeg",
-    quality: 1.0,
-  });
-
-  console.log(image);
+  // const [image, takeScreenShot] = useScreenshot({
+  //   type: "image/jpeg",
+  //   quality: 1.0,
+  // });
 
   const download = (image, { name = "img", extension = "jpg" } = {}) => {
     const a = document.createElement("a");
@@ -25,16 +24,6 @@ const Screenshot = () => {
   return (
     <div>
       <button onClick={downloadScreenshot}>Download screenshot</button>
-      {/* <div
-        ref={ref}
-        style={{
-          border: "1px solid #ccc",
-          padding: "10px",
-          marginTop: "20px",
-        }}
-      >
-        fsdfsdfs
-      </div> */}
     </div>
   );
 };
